6e7569759f015fe5582133328fab51215409bf95
Jesse F Lamb <40274208+jesseflamb@users.noreply.github.com>
2023-04-21 10:02:48 -0700

    Created Maps for At Sea Use
    Created maps and cleaned up Day to Day for at sea use

---
+++ R-Files/2023-04-17 DY23-07 Station Map for Survey Brief.R
@@ -0,0 +1,106 @@
+#Install Packages
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+packages <- c("tidyr",  "dplyr","lubridate","hms", "ggplot2", "ggmap", "lattice", "stringr", "data.table",  
+              "tibble","readxl", "sf","maps", "mapdata", "mapplots", "mapview", "marmap", "Cairo", "ncdf4",
+              "oce", "here","reshape2", "viridis", "export", "rnaturalearth", 
+              "rnaturalearthdata", "forcats","sf", "geosphere")
+ipak(packages) 
+
+# Create a base Layer map of the core sampled areas, as per JL and LR meeting on 2023-02-16
+# Create Base Layer Maps will all numbered stations labeled, with a more focused region (not whole WGOA)
+world <- ne_countries(scale = "medium", returnclass = "sf")
+class(world)
+
+
+WGOAdata <- read.csv(here("Data","WGOA station_list_dougherty_2019_ProjInstrutions.csv"))
+
+WGOAdata$Longitude..W. <- WGOAdata$Longitude..W.*-1
+# get bathymetry data
+b = getNOAA.bathy(lon1 = -166, lon2 = -140, lat1 = 52, lat2 = 62, 
+                  resolution = 15)
+## Querying NOAA database ...
+## This may take seconds to minutes, depending on grid size
+## Building bathy matrix ...
+# convert bathymetry to data frame
+bf = fortify.bathy(b)
+# get regional polygons
+reg = map_data("world2Hires")
+reg = subset(reg, region %in% c('USSR', 'USA'))
+# convert lat longs
+reg$long = (360 - reg$long)*-1
+# set map limits, whole region
+lons = c(-167, -149) #-140 = large map
+lats = c(53, 60.25) # 52 = large map
+
+######################################
+# make plot
+GOARegion_Map_New <- ggplot()+
+  # add 50m contour
+  geom_contour(data = bf, aes(x=x, y=y, z=z),breaks=c(-50),linewidth=c(0.5),colour="light grey")+
+  # add 100m contour
+  geom_contour(data = bf, aes(x=x, y=y, z=z),breaks=c(-100),linewidth=c(0.5),colour="darkgrey")+
+  # add 200m contour
+  geom_contour(data = bf, aes(x=x, y=y, z=z),breaks=c(-200),linewidth=c(0.2),colour="black")+
+  
+  geom_sf(data = world) + coord_sf(xlim = lons, ylim = lats, expand = FALSE)+
+  #Plot WGOA points
+  geom_point(data = WGOAdata, mapping = aes(Longitude..W., Latitude.N.), #, shape = Gear.Sampled
+             size = 1, show.legend = FALSE)+ 
+  #Delete "shape..." for just stations
+  #geom_text(data = WGOAdata, mapping = aes(Longitude..W., Latitude.N., label = Station ), 
+  #          nudge_y = -0.08, size = 2.5)+ 
+  scale_shape_discrete()+ 
+  theme_bw()+ xlab("Longitude")+ ylab("Latitude")+
+  theme(axis.title = element_text(size = 20))+  
+  theme(axis.text.x=element_text(size=15, color = "black"), axis.text.y = element_text(size=15, color = "black"))+
+  theme(legend.position = "none")+
+  annotate("text", x=-163,y=57.4912, label = "Bering Sea", size = 12)+
+  annotate("text", x=-153.495,y=57.4912,size = 8, label = "Kodiak Is.")+
+  annotate("text", x=-157.5,y=59.7, label = "Alaska", 
+            size = 15, fontface = "bold")
+
+GOARegion_Map_New
+
+ggsave("DY23-07 Map for Survey Brief.png",path = here("Docs"), height = 8.5, width = 11, units = "in")
+
+
+
+#### Large Station Maps for Survey  ####
+
+#### Large Overall Map ####
+
+lons = c(-164.5, -149) #-140 = large map
+lats = c(53.5, 59.5) # 52 = large map
+
+WGOALarge <- ggplot()+
+  # add 50m contour
+  geom_contour(data = bf, aes(x=x, y=y, z=z),breaks=c(-50),linewidth=c(0.5),colour="light grey")+
+  # add 100m contour
+  geom_contour(data = bf, aes(x=x, y=y, z=z),breaks=c(-100),linewidth=c(0.5),colour="darkgrey")+
+  # add 200m contour
+  geom_contour(data = bf, aes(x=x, y=y, z=z),breaks=c(-200),linewidth=c(0.2),colour="black")+
+  
+  geom_sf(data = world) + coord_sf(xlim = lons, ylim = lats, expand = FALSE)+
+  #Plot WGOA points
+  geom_point(data = WGOAdata, mapping = aes(Longitude..W., Latitude.N.), #, shape = Gear.Sampled
+             size = 1, show.legend = FALSE)+ 
+  #Delete "shape..." for just stations
+  geom_text(data = WGOAdata, mapping = aes(Longitude..W., Latitude.N., label = Station ), 
+            nudge_y = -0.08, size = 3)+ 
+  scale_shape_discrete()+
+  labs(title = "DY23-07: EcoFOCI Spring Ichthyoplankton Survey")+
+  theme_bw()+ xlab("Longitude")+ ylab("Latitude")+
+  theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 18))+
+  theme(axis.title = element_text(size = 20))+  
+  theme(axis.text.x=element_text(size=15, color = "black"), axis.text.y = element_text(size=15, color = "black"))+
+  theme(legend.position = "none")+
+  annotate("text", x=-153.495,y=57.4912,size = 6, label = "Kodiak Is.")
+ 
+WGOALarge
+ggsave("DY23-07 Map for Survey LARGE.png",path = here("Docs"), height = 8.5, width = 11, units = "in")
