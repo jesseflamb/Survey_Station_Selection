577f3b16e1c2cd7c891a79a2ce5b6982fda520f0
Jesse F Lamb <40274208+jesseflamb@users.noreply.github.com>
2023-05-21 20:43:47 -0700

    Frantic work to try to create stations on the fly
    Put this code through the wringer: had to make different simulations of station lists and maps to try create a design to get the most stations for the little time we had left on DY23-07

---
+++ R-Files/2023-05-20 Station Group Vectors for Reference.R
@@ -0,0 +1,45 @@
+### Below are Station Group Vectors for Reference when using Day to Day or Outlook
+
+# CalVET Grid, no CTDs or BONGO lines in between:
+c(119:115,127:131,141:137)
+
+# FOX Line
+c(160:165) 
+
+# Shelikof Stations, no Bs
+
+c(167,169,171,173,175,177)
+
+# NE of Kodiak Stations
+
+c(213:208,224:221,219,217,215,
+  214,216,218,220,228:225,234:238,271)
+
+#Emergency Vector Stations Whale pass
+
+c(119,117,115,124,122,120,131,129,127,136,
+  134,132,141,139,137,147,145,143,152,150,
+  148,156,155,153,158,171)
+
+c(119,117,115,124,122,120,132,
+  134,136,147,145,143,153,155,156,158,171)
+
+# Direct Route (5/21 ~ 6pm) Pass
+
+c(119,121,130,133,140,144,151,154,158,171)
+
+# Direct Route, small zigzag Whale Pass
+
+c(119,116,123,121,130,133,140,144,151,154,158,171)
+c(118,116,123,121,130,133,140,144,151,154,158,171)
+
+
+# Going alternate route (SE around Kodiak)
+
+#Classic route (transects back and forth)
+c(118,116,114,126,128,130,140,137,148,151,156)
+
+# Funnel route
+c(119,117,115,124,122,128,126,136,135,137,147,148)
+
+
